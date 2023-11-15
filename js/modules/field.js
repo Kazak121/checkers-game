f5257c395f88a09319c56cbccc99060b01309f39
Jakub_Ostach <jakubostach@gmail.com>
2023-11-15 14:11:15 +0100

    editing the class - Field

--- js/modules/field.js
+++ js/modules/field.js
@@ -1,7 +1,27 @@
 export class Field {
     #piece;
 
+    constructor(piece = null) {
+        this.piece = piece;
+    }
+    static factory(...params) {
+        return new Field(...params);
+    }
     static getDefaultPiece() {
         return new Checker();
+    }
+    set piece(obj) {
+        if (!obj instanceof Piece)
+            throw new Error('Incorrect argument')
+        this.#piece = obj;
+    }
+    get piece() {
+        return this.#piece;
+    }
+    isEmpty() {
+        return !this.piece;
+    }
+    setEmpty() {
+        this.piece = null;
     }
 }
