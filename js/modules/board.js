98b72e46331b1f5a5da931a680e31a80f67a4230
Jakub_Ostach <jakubostach@gmail.com>
2023-11-15 15:11:08 +0100

    editing board

--- js/modules/board.js
+++ js/modules/board.js
@@ -1,8 +1,66 @@
 export class Board {
-  boardRef = null;
-
+  //boardRef = null;
+  #fieldsList = [
+    ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09'], 
+    ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19'], 
+    ['20', '21', '22', '23', '24', '25', '26', '27', '28', '29'], 
+    ['30', '31', '32', '33', '34', '35', '36', '37', '38', '39'], 
+    ['40', '41', '42', '43', '44', '45', '46', '47', '48', '49'], 
+    ['50', '51', '52', '53', '54', '55', '56', '57', '58', '59'], 
+    ['60', '61', '62', '63', '64', '65', '66', '67', '68', '69'], 
+    ['70', '71', '72', '73', '74', '75', '76', '77', '78', '79'], 
+    ['80', '81', '82', '83', '84', '85', '86', '87', '88', '89'], 
+    ['90', '91', '92', '93', '94', '95', '96', '97', '98', '99']
+  ];
   constructor(boardRef) {
     this.boardRef = boardRef;
+  }
+
+  get fieldsList() {
+    const fieldsList = this.#fieldsList.map(row => {
+      return Object.freeze([...row]);
+    });
+    return Object.freeze([...fieldsList]);
+  }
+
+  static getDefaultPiece() {
+    return Field.getDefaultPiece();
+  }
+
+  init() {
+    for (let i=0; i < this.#fieldsList.length; i++) {
+      for (let j=0; j < this.#fieldsList.length; j++) {
+        const field = this.getField(i + '' + j);
+        if (!(field instanceof Field)) {
+          this.setField('${i}${j}', new Field())
+        }
+      }
+    }
+  }
+
+  get Field = function (coord) {
+    if (!this.#isCorrectCoord(coord)) {
+      throw new Error('Incorrect coords');
+    }
+    const [rowIndex, colIndex] = coords;
+    return this.#fieldsList[rowIndex][colIndex];
+  }
+
+  setField = function (coord, value) {
+    if (!this.#isCorrectCoord(coord)) {
+      throw new Error('Incorrect coords');
+    }
+    const [rowIndex, colIndex] = coord;
+    this.#fieldsList[rowIndex][colIndex] = value;
+  }
+
+  insertPieces(pieces, playerIndex) {
+    const coords = Object.keys(pieces);
+    coords.forEach(coord => {
+      const piece = pieces[coords];
+      piece.player = playerIndex;
+      this.setField(coord, Field.factory(piece));
+    })
   }
 
   initBoard() {
@@ -10,7 +68,7 @@ export class Board {
     console.log("Board initialized");
   }
 
-  createBoard() {
+/*  createBoard() {
     const boardGrid = document.createElement("div");
     boardGrid.id = "board";
 
@@ -21,14 +79,9 @@ export class Board {
     }
 
     this.boardRef.appendChild(boardGrid);
-  }
+  } */
 
-  insertPieces(pieces, playerIndex) {
-    const coords = Object.keys(pieces);
-    coords.forEach(coord => {
-      const piece = pieces[coords];
-      piece.player = playerIndex;
-      this.setField(coord, Field.factory(piece));
-    })
+  #isCorrectCoord = function (coord) {
+    return /^[0-9]{2}$/i.test(coord);
   }
 }
--- js/modules/game.js
+++ js/modules/game.js
@@ -1,15 +1,21 @@
+import { Board } from './board.js'
+import { Printer } from '.printer.js'
 export class Game {
   players_m =[];
   playersNumber_m = 2;
+  #board;
 
   constructor(modules) {
     this.board = modules.board;
   }
-  addPlayer(player)
+  addPlayer(player, pieces = [])
   {
     if (this.playersNumber_m <= this.players_m.length)
       throw new Error('Too many players!');
     this.players_m.push(player);
+    const playerIndex = this.players_m.length-1;
+    this.#board.insertPieces(pieces, playerIndex);
+    return playerIndex;
   }
  
 
