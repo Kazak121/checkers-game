6e89c4cc1d6499aee96cdb0b3fc45432e27a5e2c
Jakub_Ostach <jakubostach@gmail.com>
2023-11-15 14:33:17 +0100

    small changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import { Game } from "./modules/game.js";
 import { Board } from "./modules/board.js";
 import { Player } from '.modules.player.js';
+import { CheckersDOMPrinter } from './modules/printer.js'
 
 document.addEventListener("DOMContentLoaded", function () {
   const appContainerRef = document.getElementById("app");
@@ -9,13 +10,15 @@ document.addEventListener("DOMContentLoaded", function () {
     throw new Error("App container not found!");
   }
 
-  const board = new Board(appContainerRef);
-  const game = new Game({ board });
+  const board = new Board();
+  const printer = new CheckersDOMPrinter({ appContainerRef });
+  const game = new Game({ board, printer });
 
   const player_1 = new Player('P1');
   const player_2 = new Player('P2');
 
   game.addPlayer(player_1, Game.getStartingPositionForWhite());
+  game.addPlayer(player_2, Game.getStartingPositionForBlack());
 
   game.init();
 });
